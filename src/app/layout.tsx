10db2f474b4d4f202925686aaa919259b501b7b4
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-02-07 02:46:45 +0200

    remove define

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
       </body>
     </html>

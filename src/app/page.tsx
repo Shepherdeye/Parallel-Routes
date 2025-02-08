e6ed593278cf8c709b20898e0aac5cb0e116a811
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-02-09 01:58:48 +0200

    adding the dashboard  link withstyles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>Home Page</h1>
+      <Link href={"/dashboard"} className='text-blue-900 underline font-bold'> Dashboard</Link>
+    </div>
   )
 }
 

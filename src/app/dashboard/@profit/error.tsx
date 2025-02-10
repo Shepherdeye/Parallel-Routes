6b8cbbc7db0360f0f73ca7d43ff7bc377944fa16
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-02-10 18:38:34 +0200

    adderror page

---
+++ src/app/dashboard/@profit/error.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const OrderError = () => {
+    return (
+        <div className='card bg-red-400'>
+            Error: Failed To Fetch Profit !
+        </div>
+    )
+}
+
+export default OrderError

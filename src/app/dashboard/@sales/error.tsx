586261784819b4901386781dccc1a01176e55898
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-02-11 13:37:47 +0200

    adding  the loading  and error  pages

---
+++ src/app/dashboard/@sales/error.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const OrderError = () => {
+    return (
+        <div className='card bg-red-400'>
+            Error: Failed To Fetch Sales !
+        </div>
+    )
+}
+
+export default OrderError
---
+++ src/app/dashboard/@sales/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingOrders = () => {
+    return (
+        <div className='card loading '>Sales Loading...</div>
+    )
+}
+
+export default LoadingOrders

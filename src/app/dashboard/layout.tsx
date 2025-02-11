a183344dee0856746fb9975dd6f64f4dd3bf7202
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-02-11 13:39:48 +0200

    add it  to the layout page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,15 +4,17 @@ interface DashboardLayoutProps {
     children: ReactNode;
     orders: ReactNode;
     profit: ReactNode;
+    sales: ReactNode;
 }
 
 
-const DashboardLayout = ({ children, orders, profit }: DashboardLayoutProps) => {
+const DashboardLayout = ({ children, orders, profit, sales }: DashboardLayoutProps) => {
     return (
         <main className="p-10">
             <div className=" flex justify-center gap-7 items-center">
-                {orders}
+                {sales}
                 {profit}
+                {orders}
             </div>
             {children}
         </main>

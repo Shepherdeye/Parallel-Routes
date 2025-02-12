adccd5acbc6014a46ad640247cbc3c1bfd11c9e8
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-02-12 20:19:14 +0200

    add values

--- src/app/dashboard/@profit/default.tsx
+++ src/app/dashboard/@profit/default.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const DefaultProfits = async () => {
     const profit = await getProfitFromDB();
     return (
-        <div className='card bg-green-600'>DefaultProfits </div>
+        <div className='card bg-green-600'>DefaultProfits{profit} </div>
     )
 }
 
